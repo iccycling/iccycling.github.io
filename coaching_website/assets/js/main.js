21388e66788853743561298a84c7deef670679b3
FrederikImperial <frederik.beck23@imperial.ac.uk>
2025-08-25 00:48:00 +0200

    ok

--- coaching_website/assets/js/main.js
+++ coaching_website/assets/js/main.js
@@ -99,6 +99,6 @@ document.addEventListener('DOMContentLoaded', () => {
     if (scrollArrow) {
       scrollArrow.addEventListener('click', e => {
         e.preventDefault();
-        window.scrollBy({ top: 200, left: 0, behavior: 'smooth' });
+        window.scrollBy({ top: 400, left: 0, behavior: 'smooth' });
       });
     }
